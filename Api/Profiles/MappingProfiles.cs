e711c10c404c5960b4c019e71781b517186ac3c5
williamvg93 <sonico_5071@hotmail.com>
2023-10-31 10:25:39 -0500

    feat: :sparkles: MappingProfiles, Added Maping Profiles

---
+++ Api/Profiles/MappingProfiles.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Entities.Person;
+
+namespace Api.Profiles;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+
+    }
+}
